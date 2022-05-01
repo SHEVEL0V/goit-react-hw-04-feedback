91628a30f128155365b6f9c6d803aed4149104ea
Андрій Шевельов <ashevelevmti@gmail.com>
2022-05-01 13:52:01 +0300

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,10 @@
-import React, { Component } from 'react';
-import Statistics from './statistics';
-import Section from './title';
-import FeedbackOptions from './button';
-import options from './options.json';
+import Feadbacks from './feadbacks/feadbacks';
+import Container from './feadbacks/container';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  onIncrement = name => {
-    this.setState(st => {
-      return { [name]: st[name] + 1 };
-    });
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-  countPositiveFeedbackPercentage = () => {
-    return Math.round(
-      (this.state.good / this.countTotalFeedback()) * 100,
-    );
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <Section title="Plaese leave feedba">
-        <FeedbackOptions
-          options={options}
-          onLeaveFeedback={this.onIncrement}
-        />
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={this.countTotalFeedback()}
-          positivePercentage={this.countPositiveFeedbackPercentage()}
-        ></Statistics>
-      </Section>
-    );
-  }
+export default function App() {
+  return (
+    <Container>
+      <Feadbacks />
+    </Container>
+  );
 }
-
-export default App;
