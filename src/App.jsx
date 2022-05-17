b5b10637a254a444523cb8a89e859227593e4716
Андрій Шевельов <ashevelevmti@gmail.com>
2022-05-17 22:36:16 +0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import Section from './components/Section';
+import Statistics from './components/Statistics';
+import Title from './components/title';
+import FeedbackOptions from './components/FeedbackOptions';
+import Notification from './components/Notification';
+import data from './data/options.json';
+
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [buttons, setButtons] = useState();
+
+  useEffect(() => {
+    setButtons(data.map(({ name }) => name));
+  }, []);
+
+  const onIncrement = name => {
+    switch (name) {
+      case 'good':
+        setGood(preG => preG + 1);
+
+        break;
+      case 'neutral':
+        setNeutral(preN => preN + 1);
+        break;
+      case 'bad':
+        setBad(preB => preB + 1);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
+  };
+
+  return (
+    <>
+      <Section>
+        <Title title="Plaese leave feedba" />
+        <FeedbackOptions options={buttons} onLeaveFeedback={onIncrement} />
+      </Section>
+      <Section>
+        {!countTotalFeedback() ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          ></Statistics>
+        )}
+      </Section>
+    </>
+  );
+}
--- src/components/FeedbackOptions.jsx
+++ src/components/FeedbackOptions.jsx
@@ -2,24 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import s from './style.module.css';
 
-export default function FeedbackOptions({
-  options,
-  onLeaveFeedback,
-}) {
+export default function FeedbackOptions({ options, onLeaveFeedback }) {
   return (
     <div className={s.thumb}>
-      {options.map(el => {
-        return (
-          <button
-            key={el}
-            className={s.button}
-            name={el}
-            onClick={e => onLeaveFeedback(e.currentTarget.name)}
-          >
-            {el}
-          </button>
-        );
-      })}
+      {options &&
+        options.map(el => {
+          return (
+            <button
+              key={el}
+              className={s.button}
+              name={el}
+              onClick={e => onLeaveFeedback(e.currentTarget.name)}
+            >
+              {el}
+            </button>
+          );
+        })}
     </div>
   );
 }
